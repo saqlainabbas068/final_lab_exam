e38db176d4b506d7ab2852039c38227645fce380
Muhammad Saqlain Abbas <74317499+saqlainabbas068@users.noreply.github.com>
2021-01-09 09:18:37 +0500

    share notes

--- flutter_note_takeapp/lib/services/sharedPref.dart
+++ flutter_note_takeapp/lib/services/sharedPref.dart
@@ -1,13 +1,13 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
+Future<String> getThemeFromSharedPref() async {
+  SharedPreferences sharedPref = await SharedPreferences.getInstance();
+  return sharedPref.getString('theme');
+}
 void setThemeinSharedPref(String val) async {
   SharedPreferences sharedPref = await SharedPreferences.getInstance();
   sharedPref.setString('theme', val);
 }
 
-Future<String> getThemeFromSharedPref() async {
-  SharedPreferences sharedPref = await SharedPreferences.getInstance();
-  return sharedPref.getString('theme');
-}
 
 
